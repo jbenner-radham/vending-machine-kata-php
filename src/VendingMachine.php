52a50986c0a608c52b49996f4842237517f3f79d
James Benner <james.benner@gmail.com>
2016-02-10 14:44:02 -0500

    Added `it_should_accept_nickels()` test.

--- src/VendingMachine.php
+++ src/VendingMachine.php
@@ -2,6 +2,58 @@
 
 namespace Kata;
 
+use InvalidArgumentException;
+
 class VendingMachine
 {
+    /** @var float */
+    const NICKEL = .05;
+
+    /** @var float[]  */
+    protected $_coinage = [];
+
+    protected function _getBalance(): float
+    {
+        if (empty($this->_coinage)) {
+            return 0;
+        }
+
+        return array_sum($this->_coinage);
+    }
+
+    public function acceptCoin(float $coin)
+    {
+        switch ($coin) {
+            case self::NICKEL:
+                $this->_coinage[] = $coin;
+                break;
+
+            default:
+                throw new InvalidArgumentException('Invalid coin type received.');
+        }
+
+        return $this->checkDisplay();
+    }
+
+    public function checkDisplay(string $message = null): array
+    {
+        return ['message' => $message ?? $this->getMessage(), 'balance' => sprintf('$%.2f', $this->_getBalance())];
+    }
+
+    public function getMessage(string $message = null, float $money = null): string
+    {
+        if (!$message && !$money) {
+            if ($this->_getBalance() == 0) {
+                return 'INSERT COIN';
+            }
+
+            return sprintf('$%.2f', $this->_getBalance());
+        }
+
+        if ($money) {
+            $message .= sprintf('$%.2f', $money);
+        }
+
+        return $message;
+    }
 }
--- tests/VendingMachineSpec.php
+++ tests/VendingMachineSpec.php
@@ -2,6 +2,7 @@
 
 namespace tests\Kata;
 
+use Kata\VendingMachine;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
@@ -10,5 +11,10 @@ class VendingMachineSpec extends ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType('Kata\VendingMachine');
+    }
+
+    function it_should_accept_nickels()
+    {
+        $this->acceptCoin(VendingMachine::NICKEL)->shouldReturn(['message' => '$0.05', 'balance' => '$0.05']);
     }
 }
